95ab31bbc5216d2a8582cac066d7911174e16902
benjygarrison <46270616+benjygarrison@users.noreply.github.com>
2022-02-09 22:15:15 -0800

    testing networking bug

--- Accounts/Accounts/AccountSummary/AccountSummaryViewController+Network.swift
+++ Accounts/Accounts/AccountSummary/AccountSummaryViewController+Network.swift
@@ -16,12 +16,6 @@ struct UserProfile: Codable {
     let id: String
     let firstName: String
     let lastName: String
-    
-    enum CodingKeys: String, CodingKey {
-        case id
-        case firstName = "first_name"
-        case lastName = "last_name"
-    }
 }
 
 extension AccountSummaryViewController {
